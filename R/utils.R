8c8078ebdb4a7e653e2545c337d7cba688478497
Adam Gruer <adamgruer@icloud.com>
2019-10-23 22:27:57 +1100

    update lon_wrap_180 function
    use lwgeom package instead of sp
    make sure object returned with  original crs

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,18 @@
 lon_wrap_180 <- function(sf) {
-  sf <- sf::as_Spatial(sf)
-  sf <- sp::spTransform(sf,"+proj=longlat +datum=WGS84 +lon_wrap=180")
-  sf::st_as_sf(sf)
-}
+
+  #store original crs
+  original_crs <- sf::st_crs(sf)
+
+  # transform to projection that can handle crossing the dateline
+  sf <- lwgeom::st_transform_proj(sf,"+proj=longlat +datum=WGS84 +lon_wrap=180")
+
+  # transform back to original projection
+  sf <- lwgeom::st_transform_proj(sf, original_crs)
+
+  # set crs to original.  st_transform_proj doen't set EPSG
+  st_crs(sf) <- original_crs
+  sf
+  }
 
 is_anti_meridian_tile <- function(x, y, zoom){
   t_bbox <- slippymath::tile_bbox(x,y,zoom)
