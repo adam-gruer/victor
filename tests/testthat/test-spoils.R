fdf46c79bdf98b3f8443fb8821d9ebfc05539b41
Adam Gruer <adamgruer@icloud.com>
2019-10-24 23:33:10 +1100

    add test for spoils()

--- tests/testthat/test-spoils.R
+++ tests/testthat/test-spoils.R
@@ -1,3 +1,6 @@
-test_that("spoils with no arguments returns an object", {
-  expect_success(spoils())
+test_that("spoils with no arguments returns a list of simple
+          features data frames", {
+  expect_type(spoils(), "list")
+  expect_s3_class(spoils()[[1]], c("sf","data.frame"), exact = TRUE)
+
 })
