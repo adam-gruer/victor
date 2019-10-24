73b2ea379b13c7bff6541cb62772f4a769dbdc0a
Adam Gruer <adamgruer@icloud.com>
2019-10-24 22:42:42 +1100

    add functions to check if a tile is east or west most

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,16 @@
+#' Wrap coordinates that are on the anti-meridian (date line)
+#'
+#' features in tiles that are the east most for a zoom level
+#' (x is 2^(zoom) - 1) or the west most (x is 0) have longitudes
+#' that go from positive to negative (e.g 179 to -182). This function
+#' converts to a projection (+proj=longlat +datum=WGS84 +lon_wrap=180) that
+#' wraps these longitudes and then converts back to the original projection
+#'
+#' @param sf object
+#'
+#' @return a sf object with the same  projection as supplied
+#'
+#' @examples
 lon_wrap_180 <- function(sf) {
 
   #store original crs
@@ -10,15 +23,40 @@ lon_wrap_180 <- function(sf) {
   sf <- lwgeom::st_transform_proj(sf, original_crs)
 
   # set crs to original.  st_transform_proj doen't set EPSG
-  st_crs(sf) <- original_crs
+  sf::st_crs(sf) <- original_crs
   sf
-  }
+}
 
-is_anti_meridian_tile <- function(x, y, zoom){
-  t_bbox <- slippymath::tile_bbox(x,y,zoom)
-  t_bbox <- matrix(t_bbox, nrow = 2, byrow = TRUE)
-  t_bbox <- slippymath::merc_to_lonlat(t_bbox)
-  any(round(t_bbox[,1],10) %in% c(180.0, -180.0))
+#' Get the x index of the east most tile for a zoom level
+#'
+#' @param zoom an integer in the range 1:18
+#'
+#' @return
+#'
+#' @examples
+east_most_tile_x <- function(zoom){
+  if(any(
+         !is.integer(zoom),
+         !zoom %in% 1:18)
+         ) abort("incorrect zoom must be integer between 1 and 18 inclusive")
+
+  (2^zoom) - 1
+
+}
+
+is_east_most_tile <- function(tile, zoom){
+  if(zoom == 1) return(FALSE)
+  tile$x == east_most_tile_x(zoom)
+}
+
+is_west_most_tile <- function(tile, zoom){
+  if(zoom == 1) return(FALSE)
+  tile$x == 0
+}
+
+is_anti_meridian_tile <- function(tile,  zoom){
+  is_west_most_tile(tile, zoom) ||
+      is_east_most_tile(tile, zoom)
 }
 
 
