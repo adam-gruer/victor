7cc449253bf0b15758f463f4e457d05180670a6f
Adam Gruer <adamgruer@icloud.com>
2019-10-24 22:43:58 +1100

    add tests for utility functions and spoils()

---
+++ tests/testthat/test-spoils.R
@@ -0,0 +1,3 @@
+test_that("spoils with no arguments returns an object", {
+  expect_success(spoils())
+})
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,20 +1,32 @@
-library(sf)
 
-sf_dateline <- st_polygon(list(matrix(c(179, -43,
+
+sf_dateline <- sf::st_polygon(list(matrix(c(179, -43,
                                  -185, -43,
                                  -181, -44,
                                  179, -43),
                           ncol = 2, byrow = TRUE)))
 
-sf_dateline <- st_sfc(sf_dateline, crs = 4326)
+sf_dateline <- sf::st_sfc(sf_dateline, crs = 4326)
 
 test_that("lon_wrap_180 returns object with original crs", {
-  expect_equal(st_crs(sf_dateline),
-               st_crs(lon_wrap_180(sf_dateline)))
+  expect_equal(sf::st_crs(sf_dateline),
+               sf::st_crs(lon_wrap_180(sf_dateline)))
 
-  sf_dateline <- st_transform(sf_dateline, 3111)
-  expect_equal(st_crs(sf_dateline),
-               st_crs(lon_wrap_180(sf_dateline)))
+  sf_dateline <- sf::st_transform(sf_dateline, 3111)
+  expect_equal(sf::st_crs(sf_dateline),
+               sf::st_crs(lon_wrap_180(sf_dateline)))
 })
 
+test_that("east_most_tile_x returns correct value for all zoom levels", {
+  zoom <- 1:18
+  expect_equal(purrr::map_dbl(zoom, east_most_tile_x),
+              2^(1:18)  - 1 )
+})
 
+test_that("east_most_tile_x returns error for incorrect zoom levels", {
+  expect_error(east_most_tile_x(0))
+  expect_error(east_most_tile_x(19))
+  expect_error(east_most_tile_x(TRUE))
+  expect_error(east_most_tile_x("2"))
+  expect_error(east_most_tile_x(NA))
+})
