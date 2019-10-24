eae58a7593f1807164bdfce20225b4992a9e7767
Adam Gruer <adamgruer@icloud.com>
2019-10-25 00:17:55 +1100

    add empty test for spoils

--- tests/testthat/test-spoils.R
+++ tests/testthat/test-spoils.R
@@ -4,3 +4,10 @@ test_that("spoils with no arguments returns a list of simple
   expect_s3_class(spoils()[[1]], c("sf","data.frame"), exact = TRUE)
 
 })
+
+test_that("spoils properyl converts points in tiles that are on the
+          dateline ",{
+
+          })
+
+
